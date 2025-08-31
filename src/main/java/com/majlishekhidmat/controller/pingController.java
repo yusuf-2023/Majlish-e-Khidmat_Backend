d12e5aff2b5a979f13a91396ff2f765b83f9c05b
yusuf-2023 <145151977+yusuf-2023@users.noreply.github.com>
2025-08-31 12:14:49 +0530

    Add ping endpoint

--- src/main/java/com/majlishekhidmat/controller/pingController.java
+++ src/main/java/com/majlishekhidmat/controller/pingController.java
@@ -1,13 +1,13 @@
 package com.majlishekhidmat.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class pingController {
+@RestController  // <- Important
+public class PingController {
 
     @GetMapping("/ping")
-public String ping() {
-    return "ok";
-}
-
-
+    public String ping() {
+        return "ok";
+    }
 }
